125f99e95288df388d974512fe8c6137dab49218
Chetan Choudhary <98541541+AlfieSolomons09@users.noreply.github.com>
2023-06-02 10:41:51 +0530

    Initial Commit

---
+++ Rapsoft_Assignment/Rapsoft_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rapsoft_AssignmentApp.swift
+//  Rapsoft_Assignment
+//
+//  Created by Chetan Choudhary on 02/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Rapsoft_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
